c1bb33eb19baa0fb426e9c58504cf948f30237ef
MD. Shohag Mia <mdshohagmia14@outlook.com>
2018-05-21 13:28:01 +0600

    Added for loops

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,6 +7,10 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Simple Apps");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Number-{i}");
+            }
             Console.ReadKey();
         }
     }
